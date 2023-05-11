27596a81843c0d0f30b61e73abb5945ebd16b35a
HubertKuch <kuchhubert@gmail.com>
2023-05-12 00:26:09 +0200

    Login endpoint

--- src/main/java/com/hubert/mangocms/controllers/auth/AuthController.java
+++ src/main/java/com/hubert/mangocms/controllers/auth/AuthController.java
@@ -1,7 +1,15 @@
 package com.hubert.mangocms.controllers.auth;
 
+import com.hubert.mangocms.domain.exceptions.internal.InvalidRequestException;
+import com.hubert.mangocms.domain.requests.auth.LoginCredentials;
+import com.hubert.mangocms.domain.responses.BaseResponse;
 import com.hubert.mangocms.services.auth.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +20,12 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @PostMapping("/login/")
+    public BaseResponse baseResponse(@RequestBody LoginCredentials loginCredentials, HttpServletResponse response) throws InvalidRequestException {
+        Cookie authCookie = authService.login(loginCredentials);
+
+        response.addCookie(authCookie);
+
+        return new BaseResponse("Logged in");
+    }
 }
