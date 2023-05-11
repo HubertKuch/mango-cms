25d0f9ee4a38dbe883e19ec93f175080d5917e2d
HubertKuch <kuchhubert@gmail.com>
2023-05-12 01:30:26 +0200

    Modeled field representation

---
+++ src/main/java/com/hubert/mangocms/domain/models/blog/fields/BlogFieldRepresentation.java
@@ -0,0 +1,29 @@
+package com.hubert.mangocms.domain.models.blog.fields;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "blogs")
+@RequiredArgsConstructor
+public class BlogFieldRepresentation {
+    @Id
+    private String id = UUID.randomUUID().toString();
+    private String value;
+    @ManyToOne
+    @JoinColumn(name = "field_definition_id")
+    private BlogFieldDefinition blogFieldDefinition;
+
+    public BlogFieldRepresentation(String value, BlogFieldDefinition blogFieldDefinition) {
+        this.value = value;
+        this.blogFieldDefinition = blogFieldDefinition;
+    }
+}
