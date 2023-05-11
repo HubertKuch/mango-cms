594edd7fcac1d78d2e639fe5ff0c132f8e03277b
HubertKuch <kuchhubert@gmail.com>
2023-05-11 17:53:19 +0200

    Initial commit

---
+++ src/main/java/com/hubert/mangocms/MangoCmsApplication.java
@@ -0,0 +1,15 @@
+package com.hubert.mangocms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Vector;
+
+@SpringBootApplication
+public class MangoCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoCmsApplication.class, args);
+    }
+
+}
