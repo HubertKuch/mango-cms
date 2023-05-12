fd668ab18959a8cdb0918f50f9e7fd775257c43c
HubertKuch <kuchhubert@gmail.com>
2023-05-12 17:06:36 +0200

    Tested blog aggregator

--- src/main/java/com/hubert/mangocms/domain/models/blog/Blog.java
+++ src/main/java/com/hubert/mangocms/domain/models/blog/Blog.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @Entity(name = "blogs")
+@AllArgsConstructor
 public class Blog {
     @Id
     private String id = UUID.randomUUID().toString();
--- src/main/java/com/hubert/mangocms/repositories/blog/ApplicationBlogFieldRepresentationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hubert.mangocms.repositories.blog;
-
-import com.hubert.mangocms.domain.models.blog.fields.ApplicationBlogFieldRepresentation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationBlogFieldRepresentationRepository extends JpaRepository<ApplicationBlogFieldRepresentation, String> {}
