468423b8bba6a3d0ee75b348ecf183b2690eaa15
HubertKuch <kuchhubert@gmail.com>
2023-05-14 21:59:06 +0200

    Update blog endpoint

--- src/main/java/com/hubert/mangocms/controllers/blog/BlogController.java
+++ src/main/java/com/hubert/mangocms/controllers/blog/BlogController.java
@@ -1,10 +1,12 @@
 package com.hubert.mangocms.controllers.blog;
 
 import com.hubert.mangocms.domain.aggregators.BlogAggregator;
+import com.hubert.mangocms.domain.annotations.Restricted;
 import com.hubert.mangocms.domain.exceptions.internal.InvalidRequestException;
 import com.hubert.mangocms.domain.models.blog.Blog;
 import com.hubert.mangocms.domain.models.user.User;
 import com.hubert.mangocms.domain.requests.blog.CreateBlog;
+import com.hubert.mangocms.domain.requests.blog.UpdateBlog;
 import com.hubert.mangocms.domain.responses.AggregatedBlog;
 import com.hubert.mangocms.domain.responses.BaseResponse;
 import com.hubert.mangocms.services.blog.BlogService;
@@ -19,6 +21,7 @@ public class BlogController {
     private final BlogService blogService;
     private final BlogAggregator blogAggregator;
 
+    @Restricted
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{applicationId}/blogs/")
     public AggregatedBlog createBlog(
@@ -29,6 +32,21 @@ public class BlogController {
         return blogAggregator.aggregatedBlog(blog);
     }
 
+    @Restricted
+    @PutMapping("/{applicationId}/blogs/{blogId}/")
+    public AggregatedBlog update(
+            @PathVariable String applicationId,
+            @PathVariable String blogId,
+            @RequestAttribute User user,
+            @RequestBody UpdateBlog updateBlog
+    ) throws InvalidRequestException {
+        Blog blog = blogService.update(user, applicationId, blogId, updateBlog);
+
+        return blogAggregator.aggregatedBlog(blog);
+
+    }
+
+    @Restricted
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{applicationId}/blogs/{blogId}/")
     public BaseResponse delete(
