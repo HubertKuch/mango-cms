d358d73f019fa0ae8e1af5dfb795031f761f982c
HubertKuch <kuchhubert@gmail.com>
2023-05-16 00:06:33 +0200

    Added endpoint to fetch own data

--- src/main/java/com/hubert/mangocms/controllers/auth/AuthController.java
+++ src/main/java/com/hubert/mangocms/controllers/auth/AuthController.java
@@ -1,16 +1,15 @@
 package com.hubert.mangocms.controllers.auth;
 
+import com.hubert.mangocms.domain.annotations.Restricted;
 import com.hubert.mangocms.domain.exceptions.internal.InvalidRequestException;
+import com.hubert.mangocms.domain.models.user.User;
 import com.hubert.mangocms.domain.requests.auth.LoginCredentials;
 import com.hubert.mangocms.domain.responses.BaseResponse;
 import com.hubert.mangocms.services.auth.AuthService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +18,13 @@ final public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login/")
+    @Restricted
+    @GetMapping("/")
+    public User user(@RequestAttribute User user) {
+        return user;
+    }
+
+    @PostMapping("/")
     public BaseResponse baseResponse(
             @RequestBody LoginCredentials loginCredentials, HttpServletResponse response
     ) throws InvalidRequestException {
